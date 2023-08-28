0754e0c91d09b23da18fde32c1506745cd7476bd
malebolo41 <101300927+malebolo41@users.noreply.github.com>
2023-08-28 18:17:26 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>ProITAssist</title>
+
+
+
+  <!--Google Fonts-->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;900&family=Ubuntu:ital,wght@0,300;0,400;0,500;1,400&display=swap" rel="stylesheet">
+
+  <!-- Awesome Icons -->
+  <script src="https://kit.fontawesome.com/a90957fa17.js" crossorigin="anonymous"></script>
+
+
+  <!--styles bootstrap and JS link-->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/styles.css">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+
+
+
+</head>
+
+<body>
+
+  <section id="title">
+
+
+    <div class="container-fluid">
+
+      <!-- Nav Bar -->
+
+      <nav class="navbar navbar-expand-lg navbar-dark">
+
+        <a class="navbar-brand" href="">ProIT Assist <i class="fa-solid fa-desktop fa-lg"></i> </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#features">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#testimonials">Testimonials</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#pricing">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+
+<!-- Title -->
+
+    <div class="row">
+
+        <div class="col-lg-6">
+          <h1>Empowering Your Tech Journey, One Solution at a Time</h1>
+        </div>
+
+        <div class="col-lg-6">
+          <img class="title-img" src="images/ti.png" alt="iphone-mockup">
+        </div>
+      </div>
+
+   </div>
+
+
+  </section>
+
+
+  <!-- Features -->
+
+  <section id="features">
+
+
+ <!--<div class="container-fluid features-container"> -->
+
+
+      <div class="row">
+
+        <div class="col-lg-4 row-features">
+          <i class="features-img fa-solid fa-wrench fa-4x"></i>
+          <h3>Repairs</h3>
+          <p>Repair of all types of PC.</p>
+        </div>
+
+
+        <div class="col-lg-4 row-features">
+          <i class="features-img fa-solid fa-floppy-disk fa-4x"></i>
+          <h3>Data Recovery</h3>
+          <p>Recovery of lost data on your electronic devices.</p>
+        </div>
+
+        <div class="col-lg-4 row-features">
+          <i class="features-img fa-solid fa-camera-retro fa-4x"></i>
+          <h3>Home Security Cameras</h3>
+          <p>Installation of camera surveillance systems.</p>
+        </div>
+
+
+        <div class="col-lg-4 row-features">
+          <i class="features-img fa-solid fa-handshake-angle fa-4x"></i>
+          <h3>Remote Assistance</h3>
+          <p>Remote Assistance.</p>
+        </div>
+
+
+        <div class="col-lg-4 row-features">
+          <i class="features-img fa-solid fa-computer fa-4x"></i>
+          <h3>Computer Upgrades</h3>
+          <p>Upgrade creation depending on your budget.</p>
+        </div>
+
+        <div class="col-lg-4 row-features">
+          <i class="features-img fa-solid fa-mobile-screen-button fa-4x"></i>
+          <h3>Software and Apps</h3>
+          <p>Creation of software and mobile applications for your business or projects.</p>
+        </div>
+
+      </div>
+
+
+    <!--</div> -->
+
+  </section>
+
+
+  <!-- Testimonials -->
+
+  <section id="testimonials">
+
+
+    <div id="testimonials-carousel" class="carousel slide" data-bs-ride="false">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+
+          <h2>Thanks to ProIT assist now I can be safer thanks to the new camera system installed in my house.</h2>
+          <img class="testimonials-img" src="images/kelvin.jpg" alt="dog-profile">
+          <em>Pebbles, New York</em>
+
+        </div>
+        <div class="carousel-item">
+
+
+
+
+          <h2 class="testimonial-text">I used the ProIT Assist service and the result was incredible, good quality and in record time.</h2>
+          <img class="testimonials-img" src="images/jenny.jpg" alt="lady-profile">
+          <em>Jennifer Taveras, Yonkers, NY.</em>
+
+        </div>
+      </div>
+
+
+      <button class="carousel-control-prev" type="button" data-bs-target="#testimonials-carousel" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#testimonials-carousel" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+
+    </div>
+
+
+
+
+
+
+
+  </section>
+
+
+  <!-- Press -->
+
+
+
+
+  <!-- Pricing -->
+
+  <section id="pricing">
+
+    <h2>Contact</h2>
+    <p>You can contact us on any of our channels.</p>
+    <p>Email: KelvinTaveras@proItservices.com</p>
+    <p>Phone: (347) 502-9452</p>
+
+
+
+  </section>
+
+
+
+
+
+  <!-- Footer -->
+
+  <footer id="footer">
+    <i class="footer-icons fa-brands fa-twitter"></i>
+    <i class="footer-icons fa-brands fa-facebook-square"></i>
+    <i class="footer-icons fa-brands fa-instagram"></i>
+    <i class="footer-icons fa-solid fa-envelope"></i>
+    <p>© Copyright ProITAssist</p>
+
+  </footer>
+
+
+</body>
+
+</html>
